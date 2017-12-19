d2b6f677b9008a9fd331148cb4c6f2353e3c91ac
TAksenov <taksenov@gmail.com>
2017-12-19 11:52:30 +0500

    add copy and sorting files functional

--- homework-1/main.js
+++ homework-1/main.js
@@ -41,86 +41,41 @@ const readDir = (base, outBase, level) => {
     const files = fs.readdirSync(base);
     let firstLetterTemp = '';
     let dirTemp;
+    let tempDirFileName;
     files.forEach(item => {
         let localBase = path.join(base, item);
         let state = fs.statSync(localBase);
         if (state.isDirectory()) {
-            // console.log('    '.repeat(level) + 'Dir: ' + item);
             readDir(localBase, outBase, level + 1);
         } else {
             if (path.extname(localBase).toUpperCase() === '.MP3') {
-                console.log('---------------------------');
-                console.log('    '.repeat(level) + 'File: ' + item);
                 firstLetterTemp = path
                     .basename(localBase)
                     .slice(0, 1)
                     .toUpperCase();
                 if (dirsArr.indexOf(firstLetterTemp) === -1) {
                     dirsArr.push(firstLetterTemp);
-                    console.log(dirsArr);
-                    console.log(firstLetterTemp);
-                    // console.log(path.basename(localBase));
-                    // console.log('Такой папки нет, создай ее');
-                    // console.log(' '.repeat(level) + 'File: ' + item);
-                    // console.log(
-                    //     path.resolve('' + outBase, './' + firstLetterTemp)
-                    // );
                     dirTemp = path.resolve(
                         '' + outBase,
                         './' + firstLetterTemp
                     );
-                    let tempDirFileName = path.resolve(
-                        '' + dirTemp,
-                        './' + item
-                    );
+                    tempDirFileName = path.resolve('' + dirTemp, './' + item);
                     fs.mkdirSync(dirTemp);
-                    console.log('~~~~');
-                    console.log('' + localBase);
-                    console.log(tempDirFileName);
-                    console.log('~~~~');
-                    // fs.copyFileSync(localBase, dirTemp);
-                    // let copiedFile = fs.createReadStream(localBase);
                     fs
                         .createReadStream(localBase)
-                        .pipe(fs.createWriteStream(dirTemp));
-                    // copiedFile.pipe(fs.createWriteStream(dirTemp));
-                    // var fs = require('fs');
-
-                    // fs.createReadStream('test.log').pipe(fs.createWriteStream('newLog.log'));
-                    // console.log(copiedFile);
-                    // let bytesCopied = 0;
-                    // let fileSize = statOfDumpFile.size;
-
-                    // copiedFile.on('data', function(buffer) {
-                    //     bytesCopied += buffer.length;
-                    //     let porcentage = (bytesCopied / fileSize * 100).toFixed(2);
-                    //     if (isPrintLogs) console.log(porcentage + '%');
-                    // });
-
-                    // copiedFile.on('end', function() {
-                    //     /**
-                    //      * Отправка почты
-                    //      * @param  {[type]} mailOptions письмо
-                    //      * @param  {[type]} (error,     info          [description]
-                    //      * @return {[type]}             [description]
-                    //      */
-                    //     mailOptions.subject = 'Backup sucess';
-                    //     mailOptions.text = 'Copy of ' + currentFileName + ' to RESERV_PATH sucess. Date: ' + strDateNow;
-                    //     mailOptions.html = '<p>Copy of ' + currentFileName + ' to RESERV_PATH sucess. Date: ' + strDateNow + '</p>';
-                    //     sendEmail(mailOptions);
-                    //     if (isPrintLogs) console.log(`Copy of ${currentFileName} to RESERV_PATH sucess`);
-                    // });
+                        .pipe(fs.createWriteStream(tempDirFileName));
                 } else {
-                    // fs.copyFileSync(item, dirTemp);
-                    // console.log('Копируй файл в нужную папку');
-                    // console.log(path.basename(localBase));
-                    // console.log(' '.repeat(level) + 'File: ' + item);
-                    console.log('TEMP');
-                }
+                    dirTemp = path.resolve(
+                        '' + outBase,
+                        './' + firstLetterTemp
+                    );
+                    tempDirFileName = path.resolve('' + dirTemp, './' + item);
+                    fs
+                        .createReadStream(localBase)
+                        .pipe(fs.createWriteStream(tempDirFileName));
 
-                console.log('---------------------------');
-            } else {
-                // continue;
+                    // console.log('TEMP');
+                }
             }
         }
     });
