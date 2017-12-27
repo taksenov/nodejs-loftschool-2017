b0c744c238bbcd8733a6cc0aea8abe7594118d99
TAksenov <taksenov@gmail.com>
2017-12-27 16:25:18 +0500

    async await realization for HM-1. HM-2(part-2)

---
+++ homework-2/part-2/Parameters.js
@@ -0,0 +1,76 @@
+'use strict';
+/**
+ * Проверка параметров запуска
+ * @type {Class}
+ * return {Object} = {status:@boolean, body:@string}
+ */
+class Parameters {
+    _setParameter(checkParam, params) {
+        let checkLength, thisParam;
+        checkLength = checkParam.length;
+        thisParam = params.slice(checkLength);
+        return thisParam;
+    } //_setParameter
+
+    _checkParams(checkParam, params, message) {
+        let unknownResult;
+        for (let i = 0, maxLength = params.length; i < maxLength; i++) {
+            if (params[i].indexOf(checkParam) !== -1) {
+                switch (checkParam) {
+                    case '--help':
+                        return {
+                            status: true,
+                            body: message
+                        };
+                    // break;
+                    case '--input=':
+                        return {
+                            status: true,
+                            body: this._setParameter(checkParam, params[i])
+                        };
+                    // break;
+                    case '--output=':
+                        return {
+                            status: true,
+                            body: this._setParameter(checkParam, params[i])
+                        };
+                    // break;
+                    case '--delete':
+                        return { status: true, body: 'DELETE_INPUT_FOLDER' };
+                    // break;
+                    default:
+                        return { status: false, body: undefined };
+                    // break;
+                }
+            } else if (params[i].indexOf(checkParam) === -1) {
+                unknownResult = { status: false, body: undefined };
+            }
+        }
+        return unknownResult;
+    } //_checkParams
+
+    handleCheckHelpParam(checkParam, execParams) {
+        let obj = this._checkParams(
+            checkParam,
+            execParams,
+            `Правила использования:
+    --input=PATH_NAME    -- указывается полный путь к каталогу, который требует сортировки;
+    --output=PATH_NAME   -- указывается полный путь к каталогу, в котором будет отсортированная музыка;
+    --delete             -- удалить каталог input;
+    --help               -- справка.
+    
+    Последовательность установки параметров любая. 
+    Параметр '--help' имеет максимальный приоритет.
+    Не документированные параметры игнорируются. 
+    Сортируются файлы с расширением MP3.`
+        );
+        return obj;
+    }
+
+    handleCheckWorkParams(checkParam, execParams) {
+        let obj = this._checkParams(checkParam, execParams, '');
+        return obj;
+    }
+}
+
+module.exports = Parameters;
